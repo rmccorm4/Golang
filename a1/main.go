4103020926569d95d5f8082afc2d430081366a37
Ryan McCormick <rmccorm4@binghamton.edu>
2018-03-19 01:40:27 -0400

    almost finished read_training_file function.

--- a1/main.go
+++ a1/main.go
@@ -6,6 +6,7 @@ import (
 //	"bytes"
 	"log"
 	"os"
+	"math"
 )
 
 // function that returns byte array, and takes in a file pointer and
@@ -21,8 +22,7 @@ func readNextBytes(file *os.File, number int) []byte {
 	return bytes
 }
 
-func main() {
-	path := "training_files/1000pts_3dim.dat"
+func read_training_file(path string) {
 	
 	file, err := os.Open(path)
 	
@@ -30,8 +30,6 @@ func main() {
 		log.Fatal("Error while opening file", err)
 	}
 	defer file.Close()
-
-	fmt.Printf("%s opened\n", path)
 
 	// Cast first 8 bytes to string
 	filename := string(readNextBytes(file, 8))
@@ -41,5 +39,25 @@ func main() {
 	file_id := binary.LittleEndian.Uint64(readNextBytes(file, 8))
 	fmt.Println(file_id)
 
+	// Cast next 8 bytes to uint64
+	num_train_pts := binary.LittleEndian.Uint64(readNextBytes(file, 8))
+	fmt.Println(num_train_pts)
+	
+	// Cast next 8 bytes to uint64
+	num_train_dims := binary.LittleEndian.Uint64(readNextBytes(file, 8))
+	fmt.Println(num_train_dims)
 
+	for i := 0; uint64(i) < num_train_pts; i++ {
+		for j := 0; uint64(j) < num_train_dims; j++ {
+			uint_coord := binary.LittleEndian.Uint32(readNextBytes(file, 4))
+			point_coord := math.Float32frombits(uint_coord)
+			fmt.Println(point_coord)
+		}
+	}
+
+}
+
+func main() {
+	path := "training_files/1000pts_3dim.dat"
+	read_training_file(path)
 }
